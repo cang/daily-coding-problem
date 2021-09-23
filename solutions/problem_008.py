23f16daf7e2724a81486c54c871436f1644bcff6
Do Van Cang <dovancang@gmail.com>
2021-09-23 16:52:49 +0700

    Update problem_008.py

--- solutions/problem_008.py
+++ solutions/problem_008.py
@@ -1,3 +1,4 @@
+'''
 class Node:
     def __init__(self, data):
         self.data = data
@@ -44,3 +45,87 @@ assert count_unival_trees(node_a) == 5
 assert count_unival_trees(node_c) == 4
 assert count_unival_trees(node_g) == 1
 assert count_unival_trees(node_d) == 3
+'''
+
+#https://crazycoderzz.wordpress.com/count-the-number-of-unival-subtrees-in-a-binary-tree/
+class Node:
+    def __init__(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+
+def countUnivalSubtree(node):
+    _,count=__countUnivalSubtree(node)
+    return count
+
+def __countUnivalSubtree(node):
+    if not node:
+        return (True,0)
+    
+    leftcheck,leftcount=__countUnivalSubtree(node.left)
+    rightcheck,rightcount=__countUnivalSubtree(node.right)
+
+    if not leftcheck or not rightcheck:
+        return (False,leftcount+rightcount)
+    
+    if node.left and node.left.val!=node.val:
+        return (False,leftcount+rightcount)
+
+    if node.right and node.right.val!=node.val:
+        return (False,leftcount+rightcount)
+
+    return (True,leftcount+rightcount+1)
+
+node = Node(0,
+        Node(1),
+        Node(0,
+            Node(1,
+                Node(1),
+                Node(1)
+            ),
+            Node(0)
+        )
+    )
+assert countUnivalSubtree(node)==5
+
+node = Node(0,
+        Node(1),
+        Node(0,
+            Node(0,
+                Node(1),
+                Node(1)
+            ),
+            Node(0)
+        )
+    )
+assert countUnivalSubtree(node)==4
+
+node = Node(0,
+        Node(1),
+        Node(0,
+            Node(0,
+                Node(1),
+                Node(1)
+            ),
+            Node(0,
+                Node(1),
+                Node(1)
+            )
+        )
+    )
+assert countUnivalSubtree(node)==5
+
+node = Node(0,
+        Node(1),
+        Node(0,
+            Node(1,
+                Node(1),
+                Node(1)
+            ),
+            Node(1,
+                Node(1),
+                Node(1)
+            )
+        )
+    )
+assert countUnivalSubtree(node)==7
