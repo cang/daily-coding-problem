6e423326a05679075c4161547632dc4cd6a481f3
Do Van Cang <dovancang@gmail.com>
2021-09-24 10:46:20 +0700

    Update problem_012.py

--- solutions/problem_012.py
+++ solutions/problem_012.py
@@ -35,18 +35,17 @@ def goStairCase(n,stepsize):
 # Function to count number of ways
 # to reach s'th stair
 def countWays(n, m):
-     
     temp = 0
     res = [1]
-    
-    for i in range(1, n + 1):
-        s = i - m - 1
-        e = i - 1
+    for i in range(n):
+        s = i - m
+        #print(i,s,end=' ')
         if (s >= 0):
             temp -= res[s]
-        temp += res[e]
+        temp += res[i]
         res.append(temp)
-         
+        #print(res)
+    
     return res[n]
  
 # Driver Code
