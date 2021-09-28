6d907ee058a1254d9695543472702f603956c7a7
Do Van Cang <dovancang@gmail.com>
2021-09-28 15:28:32 +0700

    Update problem_016.py
    using circular list

--- solutions/problem_016.py
+++ solutions/problem_016.py
@@ -1,31 +1,28 @@
-class OrderLog:
-    def __init__(self, size):
-        self.log = list()
-        self.size = size
+'''
+This problem was asked by Twitter.
+You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
 
-    def __repr__(self):
-        return str(self.log)
+record(order_id): adds the order_id to the log get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N. You should be as efficient with time and space as possible.
+'''
 
-    def record(self, order_id):
-        self.log.append(order_id)
-        if len(self.log) > self.size:
-            self.log = self.log[1:]
+class LogN:
+    def __init__(self,logsize):
+        self.logsize = logsize
+        self.buffer = [None]*self.logsize
+        self.lastidx=0
 
-    def get_last(self, i):
-        return self.log[-i]
+    def record(self,order_id):
+        self.buffer[self.lastidx]=order_id
+        self.lastidx= (self.lastidx+1)%self.logsize
 
+    def get_last(self,i):
+        idx = (self.lastidx - i + self.logsize)%self.logsize
+        return self.buffer[idx]
 
-log = OrderLog(5)
-log.record(1)
-log.record(2)
-assert log.log == [1, 2]
-log.record(3)
-log.record(4)
-log.record(5)
-assert log.log == [1, 2, 3, 4, 5]
-log.record(6)
-log.record(7)
-log.record(8)
-assert log.log == [4, 5, 6, 7, 8]
-assert log.get_last(4) == 5
-assert log.get_last(1) == 8
+    def records(self,orders):
+        for x in orders:
+            self.record(x)
+
+logn = LogN(5)
+logn.records([1,2,3,4,5,6,7,8,9,10])
+assert logn.get_last(2)==9
