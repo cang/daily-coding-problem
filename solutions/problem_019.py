c2e6d77ffc822c46c1a9c85d5d3ab6c93d58fb9c
Do Van Cang <dovancang@gmail.com>
2021-09-29 20:49:07 +0700

    Update problem_019.py
    don't change original matrix

--- solutions/problem_019.py
+++ solutions/problem_019.py
@@ -1,6 +1,7 @@
 import sys
 
 
+'''
 def get_minimum_painting_cost(cost_matrix, num_houses, num_colors):
     if not cost_matrix:
         return 0
@@ -32,7 +33,44 @@ def get_minimum_painting_cost(cost_matrix, num_houses, num_colors):
         prev_house_min_index = curr_house_min_index
 
     return min(cost_matrix[num_houses - 1])
+'''
 
+def get_minimum_painting_cost(cost_matrix):
+    if not cost_matrix:
+        return 0
+        
+    num_houses=len(cost_matrix)
+    num_colors=len(cost_matrix[0])
+
+    sumhousei = [0]*num_colors
+    prev_house_min = 0
+    prev_house_min_index = -1
+    prev_house_second_min = 0    
+    for i in range(num_houses):
+
+        curr_house_min = sys.maxsize
+        curr_house_second_min = sys.maxsize
+        curr_house_min_index = 0  
+
+        for j in range(num_colors):
+
+            if j==prev_house_min_index:
+                sumhousei[j]= cost_matrix[i][j] + prev_house_second_min
+            else:
+                sumhousei[j]= cost_matrix[i][j] + prev_house_min
+
+            if curr_house_min > sumhousei[j]:
+                curr_house_second_min=curr_house_min
+                curr_house_min = sumhousei[j]
+                curr_house_min_index = j
+            elif curr_house_second_min > sumhousei[j]:
+                curr_house_second_min = sumhousei[j]
+
+        prev_house_min = curr_house_min
+        prev_house_second_min = curr_house_second_min
+        prev_house_min_index = curr_house_min_index        
+
+    return min(sumhousei)  
 
 cost_matrix = \
     [[7, 3, 8, 6, 1, 2],
