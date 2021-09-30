0834828b7f38bc49cf70f5bf7a27851cd3de6bff
Do Van Cang <dovancang@gmail.com>
2021-09-30 22:40:40 +0700

    Update problem_021.py

--- solutions/problem_021.py
+++ solutions/problem_021.py
@@ -1,3 +1,71 @@
+# not use dict
+def find_index(arr,l,r,x):
+	if l>r:
+		return -l-1
+	mid=(l+r)//2
+	if x==arr[mid]:
+		return mid
+	elif x<arr[mid]:
+		return find_index(arr,l,mid-1,x)
+	else:
+		return find_index(arr,mid+1,r,x)
+
+def min_classroom(arr):
+	arr=sorted(arr)
+	prevmax=0
+	cur_lst = [arr[0][0],arr[0][1]]
+	cur_roomlst = [1]
+
+	print(0,cur_lst,cur_roomlst,prevmax)
+
+	for i in range(1,len(arr)):
+		il=find_index(cur_lst,0,len(cur_lst)-1,arr[i][0])
+		if il<0:
+			il=-il-1
+			#out side the right of cur list
+			if il>=len(cur_lst):
+				prevmax=max(prevmax,max(cur_roomlst))
+				cur_lst=[arr[i][0],arr[i][1]]
+				break
+			else:
+				cur_lst= cur_lst[:il] + [arr[i][0]] + cur_lst[il:]
+				cur_roomlst= cur_roomlst[:il] + [cur_roomlst[il-1]] + cur_roomlst[il:]
+
+		for j in range(il):
+			prevmax=prevmax if prevmax>cur_roomlst[j] else cur_roomlst[j]
+		
+		cur_lst= cur_lst[il:]
+		cur_roomlst=cur_roomlst[il:]
+		il=0
+		
+		ir=find_index(cur_lst,0,len(cur_lst)-1,arr[i][1])
+		if ir<0:
+			ir=-ir-1
+			#out side the right of cur list
+			if ir>=len(cur_lst):
+				ir=len(cur_lst)-1
+				cur_lst=cur_lst + [arr[i][1]]
+				cur_roomlst = cur_roomlst + [1]
+			else:
+				cur_lst= cur_lst[:ir] + [arr[i][1]] + cur_lst[ir:]
+				cur_roomlst= cur_roomlst[:ir] + [cur_roomlst[ir-1]] + cur_roomlst[ir:]
+
+		for j in range(il,ir):
+			cur_roomlst[j]+=1
+
+		print(i,cur_lst,cur_roomlst,prevmax,il,ir)
+
+	return max(max(cur_roomlst),prevmax)
+
+
+a = [(30, 75), (0, 50), (60, 150)]
+print(min_classroom(sorted(a)))
+
+
+
+
+
+'''
 def get_num_classrooms(timing_tuples):
     if not timing_tuples:
         return 0
@@ -34,67 +102,5 @@ assert get_num_classrooms([(30, 75), (0, 50), (10, 60), (60, 150)]) == 3
 assert get_num_classrooms([(60, 150)]) == 1
 assert get_num_classrooms([(60, 150), (150, 170)]) == 2
 assert get_num_classrooms([(60, 150), (60, 150), (150, 170)]) == 3
+'''
 
-
-# not use dict
-def find_index(arr,l,r,x):
-	if l>r:
-		return -l-1
-	mid=(l+r)//2
-	if x==arr[mid]:
-		return mid
-	elif x<arr[mid]:
-		return find_index(arr,l,mid-1,x)
-	else:
-		return find_index(arr,mid+1,r,x)
-
-def min_classroom(arr):
-	arr=sorted(arr)
-	prevmax=0
-	cur_lst = [arr[0][0],arr[0][1]]
-	cur_roomlst = [1]
-
-	print(0,cur_lst,cur_roomlst,prevmax)
-
-	for i in range(1,len(arr)):
-		il=find_index(cur_lst,0,len(cur_lst)-1,arr[i][0])
-		if il<0:
-			il=-il-1
-			#out side the right of cur list
-			if il>=len(cur_lst):
-				prevmax=max(prevmax,max(cur_room))
-				cur_lst=[arr[i][0],arr[i][1]]
-				break
-			else:
-				cur_lst= cur_lst[:il] + [arr[i][0]] + cur_lst[il:]
-				cur_roomlst= cur_roomlst[:il] + [cur_roomlst[il-1]] + cur_roomlst[il:]
-
-		for j in range(il):
-			prevmax=prevmax if prevmax>cur_roomlst[j] else cur_roomlst[j]
-		
-		cur_lst= cur_lst[il:]
-		cur_roomlst=cur_roomlst[il:]
-		il=0
-		
-		ir=find_index(cur_lst,0,len(cur_lst)-1,arr[i][1])
-		if ir<0:
-			ir=-ir-1
-			#out side the right of cur list
-			if ir>=len(cur_lst):
-				ir=len(cur_lst)-1
-				cur_lst=cur_lst + [arr[i][1]]
-				cur_roomlst = cur_roomlst + [1]
-			else:
-				cur_lst= cur_lst[:ir] + [arr[i][1]] + cur_lst[ir:]
-				cur_roomlst= cur_roomlst[:ir] + [cur_roomlst[ir-1]] + cur_roomlst[ir:]
-
-		for j in range(il,ir):
-			cur_roomlst[j]+=1
-
-		print(i,cur_lst,cur_roomlst,prevmax,il,ir)
-
-	return max(max(cur_roomlst),prevmax)
-
-
-a = [(30, 75), (0, 50), (60, 150)]
-print(min_classroom(sorted(a)))
